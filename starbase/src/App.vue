54f7c5bf92222e8ab966f4b78eb8baa8ba42e7a6
Emerson Costa <costtasemerson@gmail.com>
2018-07-23 11:01:46 -0300

    [ADD] Add some fetch content from swapi.co api

---
+++ starbase/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <h3>{{ title }}</h3>
+        <button class="btn btn-primary" name="button" @click="fetchCharacter">Test Fetch character</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            title: "Generate your team"
+        }
+    },
+    methods: {
+        fetchCharacter() {
+            fetch('https://swapi.co/api/people/2', {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => console.log(json))
+        },
+    }
+}
+</script>
