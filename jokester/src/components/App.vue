ebe4e94a5abde671d999074356c463337c6e29a3
Emerson Costa <costtasemerson@gmail.com>
2018-08-01 21:47:30 -0300

    [ADD] Feature to remove jokes from screen

--- jokester/src/components/App.vue
+++ jokester/src/components/App.vue
@@ -2,19 +2,41 @@
     <div id="app">
         <h3>Wanna Jokes? C'mmon</h3>
         <button class="btn btn-primary" @click="initJokes" >Ten jokes random</button>
-        <div class="">
-            {{ $store.state }}
-        </div>
+        <button class="btn btn-success" @click="addJoke" >Ten jokes random</button>
+       <br>
+        <span v-for="type in types">
+            <input type="checkbox" :value='type' checked>
+            <label for="">{{type}}</label>&nbsp;
+        </span>
+       <br>
+       <div class="row">
+           <Joke
+                v-for="(joke, index) in $store.state.jokes"
+                :joke="joke"
+                :index="index"
+                key="index"
+           />
+       </div>
     </div>
 </template>
-
+ 
 <script>
 import { mapActions } from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
+    data() {
+        return {
+            types: ['general', 'knock-knock', 'programming']
+        }
+    }
     methods: mapActions([
-        'initJokes'
-    ])    
+        'initJokes',
+        'addJoke',
+    ]),
+    components: {
+        Joke  
+    }    
 }
 
 </script>
